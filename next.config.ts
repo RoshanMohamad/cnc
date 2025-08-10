56aa6fdc3baaf4e7a9df1ebd95afb13e8eadbcfa
RoshanMohamad <sarafroshan39@gmail.com>
2025-08-11 00:12:44 +0530

    refactor: Remove experimental settings and ESP32 communication logic from configuration

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    esmExternals: 'loose',
-  },
   webpack: (config, { isServer }) => {
     // MQTT.js webpack configuration for browser environment
     if (!isServer) {
@@ -23,18 +20,6 @@ const nextConfig: NextConfig = {
         os: false,
         path: false,
       };
-
-      // Handle MQTT.js imports
-      config.module.rules.push({
-        test: /\.js$/,
-        include: /node_modules\/mqtt/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      });
     }
 
     return config;
